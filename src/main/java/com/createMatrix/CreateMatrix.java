baa0a2dd0f9a1c278c5d498c2a1ab20566ea4f4c
Mateusz <smykal@engineer.com>
2021-11-12 23:48:11 +0100

    add indexes to printMatrix metod

--- src/main/java/com/createMatrix/CreateMatrix.java
+++ src/main/java/com/createMatrix/CreateMatrix.java
@@ -14,12 +14,18 @@ public class CreateMatrix implements Matrix{
         int v = matrix[0].length;
         System.out.println("v: " + v );
 
+        System.out.print("   ");
+        for (int j = 0; j < matrix[0].length; j++) {
+            System.out.print(j + "  ");
+        }
+        System.out.println();
+
         for (int i = 0; i < e; i++) {
+            System.out.print(i + "  ");
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + "  ");
             }
             System.out.println("");
-
         }
     }
 
