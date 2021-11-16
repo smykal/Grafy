24fbb539debde1210582437cc9ab6d7f261b8232
Mateusz <smykal@engineer.com>
2021-11-16 21:01:05 +0100

    create package for bfsearch

---
+++ src/main/java/com/bfs/Bfs.java
@@ -0,0 +1,8 @@
+package com.bfs;
+     //   Pop the first node from the queue
+     //   Check if the node has already been visited, if so skip it
+     //   If that node is the one we're searching for, then the search is over
+     //   Otherwise, add it to the visited nodes
+     //   Add this node's children to the queue and repeat these steps
+public class Bfs {
+}
