13ced2a9ab4ec8baffa66dcc82e01b67023d2c5e
Mateusz <smykal@engineer.com>
2021-11-17 22:42:47 +0100

    add lombok dependency

--- src/main/java/com/adjacencyList/Node.java
+++ src/main/java/com/adjacencyList/Node.java
@@ -1,9 +1,12 @@
 package com.adjacencyList;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class Node {
     char data;
-
-    public Node(char c) {
-        this.data=c;
-    }
+    int nodeNumber;
 }
